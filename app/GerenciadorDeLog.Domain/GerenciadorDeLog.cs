73eae293694066bef0f4241e06a63159aecc5613
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-01-30 08:40:16 -0200

    Adicionando documentação

--- app/GerenciadorDeLog.Domain/GerenciadorDeLog.cs
+++ app/GerenciadorDeLog.Domain/GerenciadorDeLog.cs
@@ -11,12 +11,21 @@ namespace GerenciadorDeLog.Domain
         public static string CaminhoDoLog { get; set; }
         private static Log log;
 
+        /// <summary>
+        /// Configura o caminho para onde os Logs serão salvos.
+        /// Colocar apenas o diretório. O (.txt) será criado automaticamente.
+        /// </summary>
+        /// <param string="caminho">Caminho onde os Logs serão salvos.</param>
         public static void ConfigurarCaminhoDoLog(string caminho)
         {
             CaminhoDoLog = caminho + @"\" + Assembly.GetCallingAssembly().GetName().Name + "_" + DateTime.Now.ToShortDateString().Replace(@"/","-") + ".txt";
         }
 
-
+        /// <summary>
+        /// Grava no (.txt) a situação e a descrição do Log.
+        /// </summary>
+        /// <param Situacao="situacao">Situação do Log.</param>
+        /// <param string="descricao">Frase para descrever o Log.</param>
         public static void Logar(Situacao situacao, string descricao)
         {
             log = new Log(situacao, descricao);
